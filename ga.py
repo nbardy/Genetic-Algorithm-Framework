5e9ca5b5ab532e585f4c8e0ef64696fce7cbb0d0
Nicholas Bardy <nicholasbardy@yahoo.com>
2012-03-27 17:07:27 -0400

    First commit

--- ga.py
+++ ga.py
@@ -1,34 +1,53 @@
+import random
+import bisect
+
 class geneticModel:
    @staticmethod
-   def rouletteSelcection(group):
-      #perform selection
+   def rouletteSelection(group, fitnessFunc):
+      """
+      Implements Roulette Wheel Selection
+      """
 
-      return newgroup
+      fitnessDistribution = _getDistribution(map(fitnessFunc, group))
+      #create of map of distributions to individuals
+      return _getPairs(group, fitnessDistribution)
 
    @staticmethod
-   def truncatedSelcection(group):
-      #perform selection
-      return newgroup
-
-   @staticmethod
-   def tournamentSelcection(group):
-      #perform selection
-      return newgroup
+   def rankSelection(group, fitnessFunc):
+      """
+      Implements Rank Selection
+      """
+      rankedGroup = sorted(group, lambda x,y: cmp(fitnessFunc(x),fitnessFunc(y)))
       
-   def __init__(randomIndividual, fitness, mutation, crossover, selection=None, populationSize=1000, mutationrate=0.05, parallelize=True):
+      #Can be optimized with (n+1)*n/2
+      distribution = _getDistribution(range(1, len(group) + 1))
+      
+      return _getPairs(rankedGroup, distribution)
+
+   @staticmethod
+   def truncatedSelection(group, fitnessFunc):
+      #perform selection
+
+      return newgroup
+
+   @staticmethod
+   def tournamentSelection(group, fitnessFunc):
+      #perform selection
+
+      return newgroup
+
+   def __init__(self, randomIndividual, fitness, mutation, crossover, selection=None, populationSize=1000, mutationRate=0.05):
       """Creates a new genetic algorithm model
 
       Keyword arguments:
-      randomIndividual -- a function object which returns a random individual of class individualtype
-      fitness          -- a function object which contains 
-      mutation         -- a function object which accepts an individual and returns a new mutated version
-      crossover        -- a function object which accepts two an
-      selection        -- a function object which performs selection with predefined defaults
+      randomIndividual -- a function object which returns a random individual
+      fitness          -- a function object which accepts an individual, and returns a fitness score 
+      mutation         -- a function object which accepts an individual, and mutation rate returns an individual
+      crossover        -- a function object which accepts a tuple of size 2 and returns a single individual
+      selection        -- a function object which accepts a populaion and a fitness function and returns a population of the same size
          Roulette Wheel Selection - rouletteSelection (Default)
          Truncated Selection      - truncatedSelection
          Tournament Selection     - tournamentSelection
-
-      parallelize      -- a booleab to enable/disable parallelization(Defaults to True)
       """
 
       #Initialize Object variables
@@ -36,9 +55,55 @@ class geneticModel:
       self.fitness = fitness
       self.mutation = mutation
       self.crossover = crossover
+      self.populationSize = populationSize
+      self.mutationRate = mutationRate
+
+      if selection == None:
+         selection = geneticModel.rouletteSelection
       self.selection = selection
 
-      self.population = [individual for 
+      #Create initial population
+      self.population = [self.randomIndividual() for i in xrange(populationSize)]
 
+   def advanceGeneration(self):
+      """
+      Advances the genetic Model one generation
+      """
+      #Use given selection technique for selecting parents
+      parents = self.selection(self.population, self.fitness)
 
+      #Create a child and mutate for every pair of parents
+      newgeneration = [self.mutation(self.crossover(pair), self.mutationRate) for pair in parents]
+      
+      self.population = newgeneration
 
+def _getDistribution(valueList):
+   """
+   Accepts a list of values and returns a distribution
+   
+   Example: [.3, .4, .11, .09, .03, .07] => [.3, .7, .81, .90, .93, 1]
+            [4, 5, 5, 6]                 => [.2, .45, .70, 1]
+   """
+   totalValue = sum(valueList)
+   densityList = [value/float(totalValue) for value in valueList]
+   cumValue = 0
+   distributionList = []
+
+   for value in densityList:
+      cumValue = cumValue + value
+      distributionList.append(cumValue)
+
+   return distributionList
+
+def randomFromDistribution(distribution):
+   """
+   Accepts a distribution and returns an integer representing the selected index
+   """
+   return bisect.bisect(distribution, random.random())
+
+def _getPairs(group, fitnessDistribution):
+   selectionGroup = [ ]
+   for _ in range(len(group)):
+      selectionGroup.append( ( group[randomFromDistribution(fitnessDistribution)], 
+                               group[randomFromDistribution(fitnessDistribution)] ))
+   return selectionGroup
---
+++ trial.py
@@ -0,0 +1,24 @@
+try:
+   reload(ga)
+except:
+   import ga
+
+import random
+import math
+
+# f(x) = x**2 - 12*x + 7 for x= [0,30]
+model1 = ga.geneticModel( randomIndividual = lambda: random.randint(1,5),
+                          fitness          = lambda x: x**2 - 12*x + 7,
+                          mutation         = lambda x, mutationrate: max(min( x + random.normalvariate(0,5), 30),0),
+                          crossover        = lambda (x,y): (x + y) /2,
+                          selection        = ga.geneticModel.rankSelection)
+
+#eval string of 1's and 0s
+size = 100
+model2 = ga.geneticModel( randomIndividual = lambda: ''.join([str(random.randint(0,1)) for _ in range(size)]),
+                          fitness          = lambda x: int(x, 2),
+                          mutation         = lambda x, mutationrate: ''.join(map(lambda bit: str(int(bit)^int(math.ceil(mutationrate - random.random()))), x)),
+                          crossover        = lambda (x,y): (lambda index: x[:index] + y[index:])(random.randint(0,len(x))),
+                          selection        = ga.geneticModel.rankSelection)
+
+
