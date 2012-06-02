eefe767b7f432d847dcd84ea34dda93d51930358
Nicholas Bardy <nicholasbardy@yahoo.com>
2012-06-01 21:23:20 -0400

    Add some notes

--- trial.py
+++ trial.py
@@ -1,3 +1,4 @@
+#To handle reloads in python interpreter
 try:
    reload(ga)
 except:
@@ -11,9 +12,10 @@ model1 = ga.geneticModel( randomIndividual = lambda: random.randint(1,5),
                           fitness          = lambda x: x**2 - 12*x + 7,
                           mutation         = lambda x, mutationrate: max(min( x + random.normalvariate(0,5), 30),0),
                           crossover        = lambda (x,y): (x + y) /2,
-                          selection        = ga.geneticModel.rankSelection)
+                          selection        = ga.geneticModel.rouletteSelection)
 
-#eval string of 1's and 0s
+# eval string of 1's and 0's
+# For large size this model has a exponentially slower convergence with roulette selection. Shows a good use case for rank selection over roulette
 size = 100
 model2 = ga.geneticModel( randomIndividual = lambda: ''.join([str(random.randint(0,1)) for _ in range(size)]),
                           fitness          = lambda x: int(x, 2),
